1f097f274fac68bcff0e1a42fb3a060d8f64a12a
Anshulkhambe <anshul.khambe26@gmail.com>
2024-02-01 11:33:20 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './SignIn/signin.component';
+import { LoginComponent } from './Login/login.component';
+import { WelcomepageComponent } from './WelcomePage/welcomepage.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CategoriesComponent } from './homepage/categories/categories.component';
+import { ProductcategoryComponent } from './homepage/productcategory/productcategory.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomepageComponent },
+
+  { path: 'signin', component: SigninComponent },
+  { path: 'login', component: LoginComponent },
+
+  {path:"home",component:HomepageComponent,
+  children:[
+    {path:"",component:CategoriesComponent},
+    {path:"profile",component:ProfileComponent},
+    { path: 'products/:category', component: ProductcategoryComponent }
+  
+  
+  ]
+  },
+  
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,111 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomepageComponent } from './WelcomePage/welcomepage.component';
+import { SigninComponent } from './SignIn/signin.component';
+import { LoginComponent } from './Login/login.component';
+
+import {MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { NavbarComponent } from './Navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CategoriesComponent } from './homepage/categories/categories.component';
+import { FiltersComponent } from './homepage/filters/filters.component';
+import { ProductcategoryComponent } from './homepage/productcategory/productcategory.component';
+import { ProductsComponent } from './homepage/products/products.component';
+import { UpdateprofileComponent } from './homepage/updateprofile/updateprofile.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    WelcomepageComponent,
+    SigninComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomepageComponent,
+    CategoriesComponent,
+    FiltersComponent,
+    ProductcategoryComponent,
+    ProductsComponent,
+    UpdateprofileComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatDialogModule,
+   MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+
+
+    MdbAccordionModule,
+MdbCarouselModule,
+MdbCheckboxModule,
+MdbCollapseModule,
+MdbDropdownModule,
+MdbFormsModule,
+MdbModalModule,
+MdbPopoverModule,
+MdbRadioModule,
+MdbRangeModule,
+MdbRippleModule,
+MdbScrollspyModule,
+MdbTabsModule,
+MdbTooltipModule,
+MdbValidationModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
