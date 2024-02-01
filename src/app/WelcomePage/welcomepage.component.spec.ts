3b7b6b893fbae7ad467ad5d5c0907edb453d8c82
Anshulkhambe <anshul.khambe26@gmail.com>
2024-02-01 12:13:48 +0530

    login, signin,navbar

--- src/app/WelcomePage/welcomepage.component.spec.ts
+++ src/app/WelcomePage/welcomepage.component.spec.ts
@@ -22,28 +22,3 @@ describe('WelcomepageComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { WelcomepageComponent } from './welcomepage.component';
-
-describe('WelcomepageComponent', () => {
-  let component: WelcomepageComponent;
-  let fixture: ComponentFixture<WelcomepageComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [WelcomepageComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(WelcomepageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
-
